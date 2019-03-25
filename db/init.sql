094600216655c4a0199509e2ebab3841a179cba0
Xavier Corbeil <xavier.corbeil.1@ulaval.ca>
2019-03-25 19:10:19 -0400

    correction

--- db/init.sql
+++ db/init.sql
@@ -1,16 +1,14 @@
 CREATE DATABASE memeber;
 use memeber;
 
-
 CREATE TABLE memes
 (
 	name VARCHAR(100),
-	url VARCHAR(100),
-	fid NUMERIC(3,0) primary key
+	url VARCHAR(100)
 );
 
 INSERT INTO memes
-	(name,url,fid)
+	(name,url)
 VALUES
 	("no gaz", "https://img-9gag-fun.9cache.com/photo/aKxewzj_700bwp.webp"),
 	("Probably the happiest guy alive right now <3", "https://img-9gag-fun.9cache.com/photo/a1QV4Wb_700bwp.webp"),
