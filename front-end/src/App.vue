8bc991de8bc019c5733f68c881995e3c20290ea8
Yan <yan@Yans-MacBook-Pro.local>
2019-03-31 14:24:08 -0400

    signup screen template

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,38 +1,23 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <Signup/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Signup from "./components/signup-login/Signup";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Signup
   },
-  data () {
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
---
+++ front-end/src/components/signup-login/Signup.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+    <v-layout column fill-height>
+      <v-form ref="form" v-model="valid" lazy-validation dark>
+        <v-text-field
+          v-model="username"
+          :counter="10"
+          :rules="userNameRules"
+          label="Username"
+          required
+        ></v-text-field>
+
+        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+
+        <v-text-field
+          :type="'password'"
+          v-model="password"
+          :rules="passwordRules"
+          label="Password"
+          required
+        ></v-text-field>
+
+        <v-btn color="#007ac1" @click="submit">Submit</v-btn>
+      </v-form>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    username: "",
+    userNameRules: [
+      v => !!v || "Username is required",
+      v => (v && v.length <= 10) || "Username must be less than 10 characters"
+    ],
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+/.test(v) || "E-mail must be valid"
+    ],
+    password: "",
+    passwordRules: [v => !!v || "Password is required"]
+  }),
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        console.log("patate");
+      }
+    }
+  }
+};
+</script>
