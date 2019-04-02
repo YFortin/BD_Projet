8f826032c8ccc97015e937beebcb7fd2f8d02ca3
Yan <yan@Yans-MacBook-Pro.local>
2019-04-02 19:20:09 -0400

    memes working

--- front-end/src/components/memes/Memes.vue
+++ front-end/src/components/memes/Memes.vue
@@ -11,7 +11,13 @@
               <v-card-title primary-title>
                 <h3 class="headline font-weight-bold mb-2">Start Swiping!</h3>
               </v-card-title>
-              <v-carousel v-model="carouselIndex" hide-delimiters hide-controls :cycle="false">
+              <v-carousel
+                vertical
+                v-model="carouselIndex"
+                hide-delimiters
+                hide-controls
+                :cycle="false"
+              >
                 <v-carousel-item contain v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
               </v-carousel>
             </v-card>
@@ -19,7 +25,7 @@
         </v-layout>
         <v-layout justify-center row>
           <v-flex xs1>
-            <v-btn fab @click="thumbDown">
+            <v-btn fab @click="thumbDown" :disabled="true">
               <v-icon color="pink">thumb_down_alt</v-icon>
             </v-btn>
           </v-flex>
@@ -61,10 +67,40 @@ export default {
   }),
   methods: {
     thumbUp() {
-      this.carouselIndex++;
+      this.items.shift();
+      this.addImage();
     },
     thumbDown() {
-      this.carouselIndex--;
+      this.items.shift();
+      this.addImage();
+    },
+    addImage() {
+      if (this.items.length < 3) {
+        this.items.push(
+          {
+            src:
+              "https://pay.google.com/about/static/images/social/og_image.jpg"
+          },
+          {
+            src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+          },
+          {
+            src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+          },
+          {
+            src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+          },
+          {
+            src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+          },
+          {
+            src: "https://s.hdnux.com/photos/73/53/27/15644125/6/920x920.jpg"
+          },
+          {
+            src: "https://img-9gag-fun.9cache.com/photo/aD1X1d7_700bwp.webp"
+          }
+        );
+      }
     }
   }
 };
