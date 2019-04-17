bae9aecd80f04a262c056de50a6b31cabb678368
Yan <yan@Yans-MacBook-Pro.local>
2019-04-17 09:08:40 -0400

    validation token in between pages

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -13,101 +13,109 @@ import MemerAPI from "../js/MemerAPI";
 Vue.use(Router);
 
 const router = new Router({
-  routes: [
-    {
-      path: '/',
-      redirect: '/Nav/UserProfile'
-    },
-    {
-      path: '/nav',
-      component: NavigationBar,
-      name: 'Nav',
-      redirect: 'NotFoundLogin ',
-      meta: { requiresAuth: true },
-      children: [
+    routes: [
         {
-          path: 'userprofile/:username',
-          name: 'UserProfile',
-          component: UserProfile
+            path: '/',
+            redirect: '/Nav/UserProfile'
         },
         {
-          path: 'myaccount/',
-          name: 'MyAccount',
-          component: MyAccount
+            path: '/nav',
+            component: NavigationBar,
+            name: 'Nav',
+            redirect: 'NotFoundLogin ',
+            meta: {requiresAuth: true},
+            children: [
+                {
+                    path: 'userprofile/:username',
+                    name: 'UserProfile',
+                    component: UserProfile
+                },
+                {
+                    path: 'myaccount/',
+                    name: 'MyAccount',
+                    component: MyAccount
+                },
+                {
+                    path: 'memes',
+                    name: 'Memes',
+                    component: Memes
+                },
+                {
+                    path: '*',
+                    name: 'NotFoundLogin',
+                    component: NotFound
+                }
+            ]
         },
         {
-          path: 'memes',
-          name: 'Memes',
-          component: Memes
+            path: '/notsignedin',
+            name: 'NotSignedIn',
+            component: NotLoginNavigation,
+            meta: {unsigned: true},
+            children: [
+                {
+                    path: 'signup',
+                    name: 'Signup',
+                    component: Signup
+                },
+                {
+                    path: 'login',
+                    name: 'Login',
+                    component: Login
+                },
+                {
+                    path: '*',
+                    name: 'NotfoundNotLog',
+                    component: NotFound
+                }
+            ]
         },
         {
-          path: '*',
-          name: 'NotFoundLogin',
-          component: NotFound
+            path: '*',
+            component: NotFound
         }
-      ]
-    },
-    {
-      path: '/notsignedin',
-      name: 'NotSignedIn',
-      component: NotLoginNavigation,
-      meta: { unsigned: true },
-      children: [
-        {
-          path: 'signup',
-          name: 'Signup',
-          component: Signup
-        },
-        {
-          path: 'login',
-          name: 'Login',
-          component: Login
-        },
-        {
-          path: '*',
-          name: 'NotfoundNotLog',
-          component: NotFound
-        }
-      ]
-    },
-    {
-      path: '*',
-      component: NotFound
-    }
-  ]
+    ]
 });
 
 export default router;
 
-router.beforeEach( async(to, from, next) => {
+router.beforeEach(async (to, from, next) => {
 
-  const autorization = document.cookie
-  .split(';')
-  .filter(item => item.trim().startsWith('AuthorizationMemer=')).length;
+    const autorization = document.cookie
+        .split(';')
+        .filter(item => item.trim().startsWith('AuthorizationMemer=')).length;
 
-  // const validation = await MemerAPI.User.validateToken();
+    const response = await MemerAPI.User.validateToken();
 
-  const validation = {
-    'status': 200
-  }
+    console.log(response.data.valid);
 
-  console.log(validation);
+    let validation;
 
-  if (to.matched.some(route => route.meta.requiresAuth)) {
-    if (!autorization) {
-      router.push({path: '/NotSignedIn/Login'})
-    } else if(validation.status == 200) {
-      next()
+    if (!response.data.valid) {
+        console.log("validation is false");
+        validation = false;
     } else {
-      router.push({path: '/NotSignedIn/Login'})
+        console.log("validation is true");
+        validation = true;
     }
-} else if (to.matched.some(route => route.meta.unsigned)) {
-  if (!autorization) {
-    next()
-  } else if (validation.status == 200) {
-    router.push({path: '/Nav/memes'});
-  } else {
-    router.push({path: '/NotSignedIn/Login'})
-  }
-}
+
+    if (to.matched.some(route => route.meta.requiresAuth)) {
+        if (!autorization) {
+            router.push({path: '/NotSignedIn/Login'})
+        } else if (validation) {
+            next()
+        } else {
+            document.cookie = "AuthorizationMemer= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+            router.push({path: '/NotSignedIn/Login'})
+        }
+    } else if (to.matched.some(route => route.meta.unsigned)) {
+        if (!autorization) {
+            next()
+        } else if (validation) {
+            router.push({path: '/Nav/memes'});
+        } else {
+            document.cookie = "AuthorizationMemer= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+            router.push({path: '/NotSignedIn/Login'})
+        }
+    }
 });
